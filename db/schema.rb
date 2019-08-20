b98b850b672fcf9d741c53cd1f4a5014be452eaa
Ravinder Kumar <ravinder.kumar@redblink.net>
2019-08-20 12:11:49 +0530

    new changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_061448) do
+ActiveRecord::Schema.define(version: 2019_08_20_063708) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
